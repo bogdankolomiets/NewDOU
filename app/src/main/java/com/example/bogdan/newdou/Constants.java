c7e3ec897fde0c6bc9f71c08872fa30d1ec2b055
Bogdan <kolom996@gmail.com>
2016-05-24 10:17:55 +0300

    Update Constants.java
    #1 added to constants variables for Threads.

--- app/src/main/java/com/example/bogdan/newdou/Constants.java
+++ app/src/main/java/com/example/bogdan/newdou/Constants.java
@@ -1,1 +1,11 @@
+package com.example.bogdan.newdou;
 
+public class Constants {
+	
+	public static final class DI {
+		
+		public static final String UI_THREAD = "UI_THREAD";
+		
+		public static final String EXECUTOR_THREAD = "EXECUTOR_THREAD";
+	}
+}
